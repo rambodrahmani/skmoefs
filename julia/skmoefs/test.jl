d97281ac374149212d1682b3e70806957e07c25a
Rambod Rahmani <r.rahmani@studenti.unipi.it>
2022-05-18 11:47:07 +0200

    Add normalize method developer harness test.

--- julia/skmoefs/test.jl
+++ julia/skmoefs/test.jl
@@ -15,30 +15,30 @@ include("discretization/discretizer_base.jl")
 # Fuzzy Universe
 
 # Fuzzy Singleton
-fuzzy_singleton = createSingletonFuzzySet(43.2, 3);
-println(fuzzy_singleton);
+fuzzy_singleton = createSingletonFuzzySet(43.2, 3)
+println(fuzzy_singleton)
 
-fuzzy_singletons = createSingletonFuzzySets([17.5, 24.3, 11.4, 23.1]);
+fuzzy_singletons = createSingletonFuzzySets([17.5, 24.3, 11.4, 23.1])
 for fuzzy_singleton in fuzzy_singletons
-    println(fuzzy_singleton);
+    println(fuzzy_singleton)
 end
 
 # Fuzzy Triangular Set
-triangular_fuzzy_params = [0.02777778, 0.27083333, 0.51388889, 0.75694444, 1.];
-fuzzy_triangulars = createTriangularFuzzySets(triangular_fuzzy_params, true);
+triangular_fuzzy_params = [0.02777778, 0.27083333, 0.51388889, 0.75694444, 1.]
+fuzzy_triangulars = createTriangularFuzzySets(triangular_fuzzy_params, true)
 for fuzzy_triangular in fuzzy_triangulars
-    println(fuzzy_triangular);
+    println(fuzzy_triangular)
 end
 
 # Fuzzy Trapezoidal Set
-trapezoidal_fuzzy_params = [0.02777778, 0.27083333, 0.51388889];
-trpzPrm = 0.56;
-fuzzy_trapezoidal = createTrapezoidalFuzzySet(trapezoidal_fuzzy_params, trpzPrm);
+trapezoidal_fuzzy_params = [0.02777778, 0.27083333, 0.51388889]
+trpzPrm = 0.56
+fuzzy_trapezoidal = createTrapezoidalFuzzySet(trapezoidal_fuzzy_params, trpzPrm)
 println(fuzzy_trapezoidal)
 
-trapezoidal_fuzzy_params = [0.02777778, 0.27083333, 0.51388889, 0.12345678];
-trpzPrm = 0.56;
-fuzzy_trapezoidals = createTrapezoidalFuzzySets(trapezoidal_fuzzy_params, trpzPrm, true);
+trapezoidal_fuzzy_params = [0.02777778, 0.27083333, 0.51388889, 0.12345678]
+trpzPrm = 0.56
+fuzzy_trapezoidals = createTrapezoidalFuzzySets(trapezoidal_fuzzy_params, trpzPrm, true)
 for fuzzy_trapezoidal in fuzzy_trapezoidals
     println(fuzzy_trapezoidal)
 end
@@ -46,17 +46,22 @@ end
 #########
 # TOOLBOX
 #########
-X, y, attributes, inputs, outputs = load_dataset("iris");
+
+# load dataset
+X, y, attributes, inputs, outputs = load_dataset("iris")
 println(X)
 println(y)
 println(attributes)
 println(inputs)
 println(outputs)
-println(typeof(X));
-println(typeof(y));
-println(typeof(attributes));
-println(typeof(inputs));
-println(typeof(outputs));
+println(typeof(X))
+println(typeof(y))
+println(typeof(attributes))
+println(typeof(inputs))
+println(typeof(outputs))
+
+# normalize dataset
+X_n, y_n = normalize(X, y, attributes)
 
 #############
 # DISCRETIZER
