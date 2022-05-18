5a869ef5181ae01b2ef76a7f7ffe2b57a943f337
Rambod Rahmani <r.rahmani@studenti.unipi.it>
2022-05-18 11:50:09 +0200

    Implement train_test_split using ScikitLearn.jl.

--- julia/skmoefs/example.jl
+++ julia/skmoefs/example.jl
@@ -4,6 +4,7 @@
 
 include("toolbox.jl")
 using Random
+using ScikitLearn.CrossValidation: train_test_split
 
 function test1()
 end
@@ -18,6 +19,8 @@ function test3(dataset::String, algorithm::String, seed::Int64, nEvals::Int64=50
 
     X, y, attributes, inputs, outputs = load_dataset(dataset)
     X_n, y_n = normalize(X, y, attributes)
+
+    Xtr, Xte, ytr, yte = train_test_split(X_n, y_n, test_size=0.3, random_state=seed)
 end
 
 test3("iris", "mpaes22", 2, 2000, true)
