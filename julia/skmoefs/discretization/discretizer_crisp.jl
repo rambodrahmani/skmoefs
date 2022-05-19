5e540ac6e352336eef7c73ebfe9e80b3f859b81f
Yuli Orozco <y_orozco@hotmail.com>
2022-05-19 10:44:11 +0200

    Start crisp discretizer

--- julia/skmoefs/discretization/discretizer_crisp.jl
+++ julia/skmoefs/discretization/discretizer_crisp.jl
@@ -19,6 +19,39 @@ end
 
 CrispMDLFilter() = CrispMDLFilter()
 
+function __init__(self::CrispMDLFilter, numClasses::Int64,  data::Matrix{Float64}, 
+    label::Array{Int64}, continuous::Array{Bool}, minNumExamples::Int64 = 2, 
+    minGain::Float64 = 0.01)
+
+    """Class for performing crisp discretization of the dataset.
+
+    Attributes
+    ----------
+    numClasses: int
+        Number of classes in the dataset.
+    data: np.array, shape (numSamples, numFeatures)
+        N samples, M features
+    label: np.array, shape (numSamples)
+        Class label for each sample.
+    continous: list, shape(numFeatures)
+        True for each continous feature, False for each categorical feature.
+    minNumExamples: int
+        Minimum number of examples per node.
+    minGain: float
+        Minimum entropy gain per spit.
+
+    """
+    self.numClasses = numClasses
+    self.continous = continous
+    self.minNumExamples = minNumExamples
+    self.data = data
+    self.label = label
+    self.minGain = minGain
+    self.candidateSplits = isnothing
+    self.cutPoints = isnothing
+    
+end
+
 function run(self::CrispMDLFilter) 
     """
     Parameters
