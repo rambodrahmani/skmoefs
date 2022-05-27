e3444423e020f3af6b38a2e5ae7fe34fa6891727
Rambod Rahmani <r.rahmani@studenti.unipi.it>
2022-05-27 16:29:55 +0200

    Initial RCSVariator implementation.

--- julia/skmoefs/rcs.jl
+++ julia/skmoefs/rcs.jl
@@ -5,6 +5,41 @@
 
 include("fmdt.jl")
 
+mutable struct RCSVariator
+    p_crb::Float64
+    p_cdb::Float64
+    alpha::Float64
+    p_mrb::Float64
+    p_mrb_add::Float64
+    p_mrb_del::Float64
+    p_mrb_flip::Float64
+    p_mdb::Float64
+    p_cond::Float64
+end
+
+RCSVariator() = RCSVariator(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+function __init__(self::RCSVariator, p_crb::Float64=0.2, p_cdb::Float64=0.5, alpha::Float64=0.5,
+                p_mrb::Float64=0.1, p_mrb_add::Float64=0.5, p_mrb_del::Float64=0.2,
+                p_mrb_flip::Float64=0.7, p_mdb::Float64=0.2, p_cond::Float64=0.5)
+    self.p_crb = p_crb
+    self.p_cdb = p_cdb
+    self.alpha = alpha
+    self.p_mrb = p_mrb
+    self.p_mrb_add = p_mrb_add
+    self.p_mrb_del = p_mrb_del
+    self.p_mrb_flip = p_mrb_flip
+    self.p_mdb = p_mdb
+    self.p_cond = p_cond
+end
+
+function createRCSVariator(p_crb::Float64=0.2, p_cdb::Float64=0.5, alpha::Float64=0.5,
+                    p_mrb::Float64=0.1, p_mrb_add::Float64=0.5, p_mrb_del::Float64=0.2,
+                    p_mrb_flip::Float64=0.7, p_mdb::Float64=0.2, p_cond::Float64=0.5)
+    return __init__(RCSVariator(), p_crb, p_cdb, alpha, p_mrb, p_mrb_add, p_mrb_del,
+                    p_mrb_flip, p_mdb, p_cond)
+end
+
 mutable struct RCSInitializer
     discretizer::FuzzyDiscretization
     tree::FMDT
